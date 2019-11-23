313f0887b1f12a902b7390a5b4446f06850f82dc
Thai Le <thailekha@yahoo.com>
2019-11-23 04:40:17 +0000

    Updates

---
+++ precache-manifest.8282cef8613c9ad3858a208e0ee695bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "329790b85499e72215b490261c00b2d0",
+    "url": "https://madmuc2018.github.io/cd/static/media/logo.329790b8.png"
+  },
+  {
+    "revision": "bfc041464366bfe41460b618b02e7a66",
+    "url": "https://madmuc2018.github.io/cd/static/media/consent.bfc04146.pdf"
+  },
+  {
+    "revision": "b46efa812859a321af6f",
+    "url": "https://madmuc2018.github.io/cd/static/js/runtime~main.ed982898.js"
+  },
+  {
+    "revision": "a8c15e7b6326d9bfa71f",
+    "url": "https://madmuc2018.github.io/cd/static/js/main.293b7ff5.chunk.js"
+  },
+  {
+    "revision": "b29f8ea24faeab2166dc",
+    "url": "https://madmuc2018.github.io/cd/static/js/2.e70335b4.chunk.js"
+  },
+  {
+    "revision": "a8c15e7b6326d9bfa71f",
+    "url": "https://madmuc2018.github.io/cd/static/css/main.033ddfa2.chunk.css"
+  },
+  {
+    "revision": "b29f8ea24faeab2166dc",
+    "url": "https://madmuc2018.github.io/cd/static/css/2.229dd9c1.chunk.css"
+  },
+  {
+    "revision": "8c143aafa2e7c48c518d8d4460249808",
+    "url": "https://madmuc2018.github.io/cd/index.html"
+  }
+];
